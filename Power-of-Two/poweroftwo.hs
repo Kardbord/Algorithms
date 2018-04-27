0afef01f0a6099246a3fabdc2be2fefc8ab087d5
Kardbord <tanner.kvarfordt@gmail.com>
2018-04-26 21:07:21 -0600

    Implemented poweroftwo in haskell using the bitwise method

--- Power-of-Two/poweroftwo.hs
+++ Power-of-Two/poweroftwo.hs
@@ -1,4 +1,5 @@
 module PowerOfTwo where
+import Data.Bits
 
 -- Determines if @n is a power of two
 --
@@ -10,7 +11,21 @@ module PowerOfTwo where
 --
 -- param n : any number
 -- return  : true if @n is a power of two, false if not
-poweroftwo :: Integer -> Bool
-poweroftwo n = 
+poweroftwoLog :: Integer -> Bool
+poweroftwoLog n = 
     fromIntegral (floor (logBase 2 (fromInteger n)) :: Integer) == (logBase 2 (fromInteger n))
+
+-- Determines if @n is a power of two
+--
+-- Determines if @n is a power of two by
+-- taking advantage of the fact that
+-- powers of two have only one bit flipped.
+-- Hence, if the result of a bitwise 'and'
+-- operation on @n and @n-1 results in 0,
+-- then @n is a power of two.
+--
+-- param n : any whole number
+-- return  : true if @n is a power of two, false if not
+poweroftwo :: Integer -> Bool
+poweroftwo n = ((n - 1) .&. n) == 0
 
